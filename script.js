3a561238f93d94ae0133ec8631f6d69807e6b77d
JessicaHa12 <jessica.ha1@ocsbstudent.ca>
2021-01-07 16:06:48 +0000

    new line

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const x = document.getElementById('myBtn')
 x.addEventListener('click', theCircumference)
 x.addEventListener('click', theArea)
 function theCircumference () {
-  let radius = document.getElementById('Radius').value
+  let radius = document.getElementById('rAdius').value
   radius = parseInt(radius)
-  answer = 2 * Math.PI * radius;
+  answer = 2 * Math.PI * radius
   document.getElementById('circumference').innerHTML = answer
 }
 function theArea () {
-  let radius = document.getElementById('areaRadius').value
+  const radius = document.getElementById('areaRadius').value
   radius = parseInt(radius)
-  let areaAnswer = Math.PI * radius * radius;
+  let areaAnswer = Math.PI * radius * radius
   document.getElementById('area').innerHTML = areaAnswer
 }
